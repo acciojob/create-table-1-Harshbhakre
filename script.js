bb60812d33a7139a993f87779b957bf31fa696a3
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-21 12:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let parent  = document.querySelector("#sampleTable")
 function insert_Row() {
 let newData = document.createElement("tr")
-newData.innerHTML = `<tr><td>New Cell1</td><td>New Cell2</td></tr>`
+newData.innerHTML = `<td>New Cell1</td><td>New Cell2</td>`
 	parent.insertBefore(newData, parent.firstChild)
 }
