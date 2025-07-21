b164d9190fc83c5ee06a0270db745751bc966d02
Harsh Bhakre <140039147+Harshbhakre@users.noreply.github.com>
2025-07-21 12:11:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let parent  = document.querySelector("#sampleTable")
 
 function insert_Row() {
 let newData = document.createElement("tr")
-newData.innerHTML = `<tr><td>New cell1</td> 
-		<td>New cell2</td></tr>`
+newData.innerHTML = `<tr><td>New Cell1</td> 
+		<td>New Cell2</td></tr>`
 	parent.insertBefore(newData, parent.firstChild)
 }
